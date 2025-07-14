c1c509ce1c10fd26894f195948b0edf983006c0b
Sebastian <Sebastian@algirdo.com>
2025-07-14 01:03:38 -0400

    Add comment to lockdown attribute in CityState for clarification

--- findpatientzero/engine/entities/city.py
+++ findpatientzero/engine/entities/city.py
@@ -20,7 +20,7 @@ class CityState:
     alerted: bool = False
     """Whether the city has been alerted to an epidemic."""
 
-    lockdown: int = 0
+    lockdown: int = 0 #QUESTION, is this implmented, and should it restric players. 
     """The number of rounds of lockdown remaining."""
 
     infection_pause: int = 0
